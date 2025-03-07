33529b2280976c838e84a8c54472e92488ca5772
3038ICERobotics <3038robotics@isd138.org>
2025-03-06 18:17:36 -0600

    Reorganized CAN IDs
    Reorganized CAN IDs

--- src/main/java/frc/robot/MotorIDs.java
+++ src/main/java/frc/robot/MotorIDs.java
@@ -1,23 +1,23 @@
 package frc.robot;
 
 public class MotorIDs {
-    public static int ElevatorLeftID = 19;
-    public static int ElevatorRightID = 20;
-    public static int AlgaeBooterID = 2;
-    public static int OuttakeRollerID = 3;
-    public static int FrontLeftSteerID = 4;
+    public static int FrontLeftDriveID = 2;
+    public static int FrontLeftSteerID = 3;
+    public static int FrontRightDriveID = 4;
     public static int FrontRightSteerID = 5;
-    public static int BackLeftSteerID = 6;
-    public static int BackRightSteerID = 7;
-    public static int HookID = 8;
-    public static int IntakeRollerID = 9;
-    public static int AngleLeftID = 10;
-    public static int AngleRightID = 11;
-    public static int FrontLeftDriveID = 12;
-    public static int FrontRightDriveID = 13;
-    public static int BackLeftDriveID = 14;
-    public static int BackRightDrive = 15;
-    public static int AlgaeLeftID = 16;
-    public static int AlgaeRightID = 17;
-    public static int AlgaeFlexID = 18;
+    public static int BackLeftDriveID = 6;
+    public static int BackLeftSteerID = 7;
+    public static int BackRightDrive = 8;
+    public static int BackRightSteerID = 9;
+    public static int ElevatorLeftID = 10;
+    public static int ElevatorRightID = 11;
+    public static int AlgaeBooterID = 12;
+    public static int OuttakeRollerID = 13;
+    public static int HookID = 14;
+    public static int IntakeRollerID = 15;
+    public static int AngleLeftID = 16;
+    public static int AngleRightID = 17;
+    public static int AlgaeLeftID = 18;
+    public static int AlgaeRightID = 19;
+    public static int AlgaeFlexID = 20;
 }
